de00dd6e5ff203eae7efe4541925608e1d6fafa7
C89306 <c89306@C02WF134HTD7.tld>
2020-02-05 10:39:15 -0500

    use author name instead of id to write to json

--- read-submission.py
+++ read-submission.py
@@ -9,11 +9,11 @@ data = {}
 with open(csvFilePath, mode="r", encoding="utf-8-sig") as csvFile:
     csvReader = csv.DictReader(csvFile)
     for csvRow in csvReader:
-        id = csvRow['id']
+        id = csvRow['author']
         data[id] = csvRow
 
         with open(jsonFilePath, 'w') as jsonFile:
-            jsonFile.write(json.dumps(data['1'], indent=4))
+            jsonFile.write(json.dumps(data[id], indent=4))
 
         authorName = csvRow['author'].replace(' ','_').lower()
         os.system('hugo new --kind authors authors/' + authorName)
